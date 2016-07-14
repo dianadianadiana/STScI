5dbcfd10472586003d30cba5c629faa5dd6bac84
Diana Kossakowski <dkossakowski@itsd-summer06.st-guest.org>
2016-07-14 08:42:24 -0400

    Essentially the same as FluxFittingScipy.py except that this file was the first 'copy' and uses Parameters and the lmfit module, whereas FluxFittingScipy.py uses scipy.optimize and is much more accurate than using lmfit (maybe because the function is complicated)

--- LFlat/FluxFitting.py
+++ LFlat/FluxFitting.py
@@ -8,6 +8,7 @@ CHIP1YLEN = CHIP2YLEN = 2048
 ###########################################
 ###########################################
 ###########################################
+
 from sympy import * 
 def norder2dpoly(n):
         ''' 
@@ -49,6 +50,7 @@ def norder2dpoly(n):
 from astropy.table import Table, Column
 from DataInfoTab import remove_stars_tab, convertmag2flux, convertflux2mag,\
                         make_avgmagandflux, sigmaclip_delmagdelflux
+
 # Read in the data
 path = '/Users/dkossakowski/Desktop/Data/'
 datafil = 'wfc_f606w_r5.lflat'
@@ -64,17 +66,18 @@ types = [int, int, int, np.float64, np.float64, np.float64, np.float64,
 tab = Table(data, names=names, dtype=types)
 tab.remove_columns(['filenum','d1','d2','d3'])   # remove the dummy columns  
 tab.sort(['id'])                                 # sort the table by starID
-tab = tab[10000:10050]
+chosen = np.random.choice(len(tab), 7000, replace = False)
+tab = tab[chosen]
 starIDarr = np.unique(tab['id'])                 # collect all the star IDs
 
-datafil = 'one_noise.txt'
-data = np.genfromtxt(path+datafil)
-names = ['id', 'chip', 'x', 'y', 'mag', 'magerr']
-types = [int, int, np.float64, np.float64, np.float64, np.float64]
-
-tab = Table(data, names = names, dtype = types)
-tab.remove_row(0)
-starIDarr = np.unique(tab['id'])     # collect all the star IDs
+#datafil = 'realistic2.dat'
+#data = np.genfromtxt(path+datafil)
+#names = ['id', 'chip', 'x', 'y', 'mag', 'magerr']
+#types = [int, int, np.float64, np.float64, np.float64, np.float64]
+#
+#tab = Table(data, names = names, dtype = types)
+#tab.remove_row(0)
+#starIDarr = np.unique(tab['id'])     # collect all the star IDs
 
 tab =  tab[np.where((tab['mag'] <= 25) & (tab['mag'] >= 13))[0]] # (13,25)
 print 'Len of tab after constraining the magnitudes'
@@ -91,8 +94,8 @@ tab =  tab[np.where(tab['flux']/tab['fluxerr'] > 5)[0]] # S/N ratio for flux is 
 ###########################################
 ############ Function to Fit ##############
 ###########################################
-
-func2read, func2fit = norder2dpoly(0)
+n = 1
+func2read, func2fit = norder2dpoly(n)
 print func2read
 func2string = np.copy(func2read)
 k = 0
@@ -101,6 +104,62 @@ while k < len(func2string):
     func2string[k] = func2string[k].replace('*','')
     k+=1
 
+def getfit(tab, xpixel, ypixel, chipnum, n = 5):
+    '''
+    NEED TO ADD DESCRIPTION
+    '''
+    starsinboth = np.array([])
+    for star in starIDarr:
+        starrows = tab[np.where(tab['id']==star)[0]]
+        chipavg = np.mean(starrows['chip'])
+        if chipavg != 1.0 and chipavg != 2.0:
+            starsinboth = np.append(starsinboth, star)
+    rows = [row for row in tab if row['chip'] == chipnum or (row['id'] in starsinboth and row['chip'] != chipnum)]
+
+    x = [row['x'] for row in rows]
+    y = [row['y'] if  row['chip'] == 2 else row['y'] + CHIP2YLEN for row in rows]
+    z = [row['flux'] - row['avgflux'] for row in rows]
+    
+    x = np.asarray(x)
+    y = np.asarray(y)
+    z = np.asarray(z)
+    
+    xx, yy = np.meshgrid(xpixel, ypixel, sparse = True, copy = False) #why copy false??
+    f = func2fit(x,y)
+    fmesh = func2fit(xx,yy)
+    try:
+        f[0] = np.ones(len(x))
+        fmesh[0] = np.ones(len(xx))
+    except TypeError:
+        pass
+    
+    A = np.array(f).T
+    B = z
+    coeff, rsum, rank, s = np.linalg.lstsq(A, B)
+    zfit = np.zeros(len(x))
+    zzfit = [[0 for i in xpixel] for j in ypixel]
+    k = 0
+    while k < len(coeff):
+        zfit += coeff[k]*f[k]
+        zzfit += coeff[k]*fmesh[k]
+        k+=1
+    # Examples:
+    # zfit = coeff[0]*x**2 + coeff[1]*y**2 + ... === coeff[0]*f[0] + ...
+    # Zfit = coeff[0]*X**2 + coeff[1]*Y**2 + ... === coeff[0]*fmesh[0] + ...
+    
+    def get_res(z, zfit):
+        # returns an array of the error in the fit
+        return np.abs(zfit - z)
+    resarr = get_res(z, zfit)
+    return [x, y, z, zfit, xx, yy, zzfit, coeff, rsum, resarr]
+xbin = 10
+ybin = 5
+xpixel1 = np.linspace(0, CHIP1XLEN, xbin)
+ypixel1 = np.linspace(CHIP2YLEN, CHIP1YLEN + CHIP2YLEN, ybin)
+xpixel2 = np.linspace(0, CHIP2XLEN, xbin)
+ypixel2 = np.linspace(0, CHIP2YLEN, ybin)
+x1, y1, z1, zfit1, xx1, yy1, zzfit1, coeff1, rsum1, resarr1 = getfit(tab, xpixel1, ypixel1, chipnum = 1, n = n)
+x2, y2, z2, zfit2, xx2, yy2, zzfit2, coeff2, rsum2, resarr2 = getfit(tab, xpixel2, ypixel2, chipnum = 2, n = n)
 ###########################################
 ####### Setting up the Parameters #########
 ###########################################      
@@ -132,26 +191,12 @@ paramsb.add_many(
 
 initialvalarra = np.zeros(len(func2string))
 initialvalarrb = np.zeros(len(func2string))
-initialvalarra[0] = initialvalarrb[0] = 1
-initialvalarra2 = [ -4.98347182e+00 ,  3.38102878e-04 ,  3.23045209e-03  , 8.47315131e-08,
-  -1.78686557e-07 , -5.05119015e-07]
-initialvalarrb2 = [ -1.41263472e-01 ,  1.35223560e-04  , 1.08437470e-04 ,  1.02392897e-07,
-  -2.29799058e-07 ,  1.20291872e-08]
-initialvalarra5 = np.asarray([ -1.64584435e-11,   4.97169658e-09 , -1.12197506e-08 , -1.22292028e-05,
-   1.03423671e-05 , -1.71735331e-06,   6.92467115e-09,  1.70430321e-09,
-  -5.79151447e-09  , 1.12096367e-09,  -2.28116739e-12,   6.62550707e-14,
-  -2.14688036e-13   ,1.20419983e-12,  -2.41487777e-13,   1.71972376e-16,
-   2.51453023e-16  ,-2.62636118e-16,   1.01128111e-16, -9.93518825e-17,
-   1.72057803e-17])
-initialvalarrb5 = np.asarray([  3.64125186e-06,   1.48570623e-03 , -3.39144333e-04 , -6.15906753e-06,
-   1.52775045e-06,  -3.49543054e-06,   9.46819175e-09,  -2.65781172e-09,
-   1.53540658e-09,   5.90413613e-09,  -6.03073373e-12,   2.59840035e-12,
-  -9.77822378e-13,  -1.58412464e-12,  -3.20367591e-12,   1.35366710e-15,
-  -8.74887202e-16,   5.43835810e-16,  -2.02095285e-16,   6.08237419e-16,
-   5.45353693e-16])
-   
+initialvalarra[0] = initialvalarrb[0] = 1e-20
+
 initialvalarra = initialvalarra
 initialvalarrb = initialvalarrb
+initialvalarra = zfit1
+initialvalarrb = zfit2
 
 for elem, initialvala, initialvalb in zip(func2string, initialvalarra, initialvalarrb):
     paramsa.add('a' + elem, initialvala, True, None, None, None)
@@ -170,8 +215,7 @@ def chisqstar(starrows, params):
         pardict = params.valuesdict()
         return np.asarray(pardict.values())
     def getfitvalue(chipnum, x, y, pointvalue = True):
-        if chipnum == 1 and chip2fit == 2: y = y + CHIP2YLEN 
-        if chipnum == 2 and chip2fit == 1: y = y - CHIP2YLEN
+        if chipnum == 1: y += CHIP2YLEN
         funcvalues = func2fit(x,y)
         if not pointvalue:
             funcvalues[0] = np.ones(len(x))
@@ -182,19 +226,23 @@ def chisqstar(starrows, params):
     fits = [getfitvalue(row['chip'], row['x'], row['y']) for row in starrows]
     avgf = np.mean(starfluxes/fits)
     starresid = (starfluxes/fits - avgf)/(starfluxerrs/fits) # currently an Astropy Column
-    #print 'starfluxes', starfluxes
-    #print 'errors in star flux', starfluxerrs
-    #print 'fits', fits
-    #print 'avgf', avgf
     return np.asarray(starresid).tolist()
    
 def chisqall1(params, func2fit, tab, chip2fit):
     starIDarr = np.unique(tab['id'])
+    stars2consid = starIDarr
+    stars2consid = np.array([])
+    for star in starIDarr:
+        starrows = tab[np.where(tab['id']==star)[0]]
+        chipavg = np.mean(starrows['chip'])
+        if (chipavg != 1.0 and chipavg != 2.0) or (int(chipavg) == int(chip2fit)):
+            stars2consid = np.append(stars2consid, star)
+            
     # np.where(tab['id'] == star)[0]                -- the indexes in tab where a star is located
     # tab[np.where(tab['id'] == star)[0]]           -- "starrows" = the rows of tab for a certain star
     # chisqstar(tab[np.where(tab['id'] == star)[0]])-- the chi squared for just one star
     #totalsum = np.sum([chisqstar(tab[np.where(tab['id'] == star)[0]]) for star in starIDarr])
-    totalresid = np.asarray([chisqstar(tab[np.where(tab['id'] == star)[0]], params) for star in starIDarr])
+    totalresid = np.asarray([chisqstar(tab[np.where(tab['id'] == star)[0]], params) for star in stars2consid])
     totalresid = reduce(lambda x, y: x + y, totalresid) # flatten totalresid
     #print np.sum(np.asarray(totalresid)**2)
     #print totalresid
@@ -378,16 +426,18 @@ def convert2mesh(chipnum, resultparams, xbin = 10, ybin = 5, wave = False):
         ypixel = np.linspace(0, CHIP2YLEN, ybin)
     else: # chipnum = 1
         xpixel = np.linspace(0, CHIP1XLEN, xbin)
-        #ypixel = np.linspace(0, CHIP2YLEN + CHIP1YLEN, ybin) 
-        ypixel = np.linspace(0, CHIP1YLEN, ybin) 
+        ypixel = np.linspace(CHIP2YLEN, CHIP2YLEN + CHIP1YLEN, ybin) 
+        #ypixel = np.linspace(0, CHIP1YLEN, ybin) 
     xx, yy = np.meshgrid(xpixel, ypixel, sparse = True, copy = False) #why copy false??
     if not wave: # if it's a polynomial
         fmesh = func2fit(xx,yy)
-        if chipnum == 1:
-            yy = yy + CHIP2YLEN
+        #if chipnum == 1:
+        #    yy = yy + CHIP2YLEN
         resultdict = resultparams.valuesdict()
         coeff = np.asarray(resultdict.values())
         zzfit = [[0 for i in xpixel] for j in ypixel]
+        print coeff
+        print fmesh
         k = 0
         while k < len(coeff):
             zzfit += coeff[k]*fmesh[k]
@@ -432,13 +482,15 @@ def plotimg(title = ''):
     ax = fig.add_subplot(111)
     ax.set_title(title)
     ax.set_xlabel('X Pixel', fontsize = 18);  ax.set_ylabel('Y Pixel', fontsize = 18)
-    extent=(0,4096,0,4096)    
+    extent=(0, 4096, 0, 4096)    
     cax = ax.imshow(np.double(imga), cmap = 'gray_r', interpolation='nearest', origin='lower', extent=extent)
     cax = ax.imshow(np.double(imgb), cmap = 'gray_r', interpolation='nearest', origin='lower', extent=extent)
     fig.colorbar(cax, fraction=0.046, pad=0.04)
     return fig
-imgbin = plotimg(title = 'Average delta flux in bins')
+    
+imgbin = plotimg(title = 'Average delta flux in bins (parameter fitting)')
 plt.show(imgbin)
+
 def plotall(tab, a,b):
     X1,Y1,Z1 = a
     X2,Y2,Z2 = b
@@ -455,3 +507,19 @@ def plotall(tab, a,b):
     plt.legend()
     return fig
 plt.show(plotall(tab,convert2mesh(chipnum=1, resultparams=resparamsa), convert2mesh(chipnum=2, resultparams=resparamsb)))
+
+def plot3dfit2chips(x1,y1,z1,X1,Y1,Z1, x2,y2,z2,X2,Y2,Z2,title = '', scatter = False):    
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_wireframe(X1,Y1,Z1, rstride=1, cstride=1,color='red', label = "CHIP1")
+    ax.plot_wireframe(X2,Y2,Z2, rstride=1, cstride=1,color='blue', label = "CHIP2")
+    ax.set_title(title)
+    if scatter:
+        ax.scatter(x1,y1,z1, s= 2, alpha = .5, c='red')
+        ax.scatter(x2,y2,z2, s= 2, alpha = .5, c='blue')
+    ax.set_xlabel('X Pixel')
+    ax.set_ylabel('Y Pixel')
+    plt.legend()
+    #ax.set_zlim([-.1,.1])
+    return fig
+plt.show(plot3dfit2chips(x1,y1,z1,xx1,yy1,zzfit1, x2,y2,z2,xx2,yy2,zzfit2,title = '', scatter = True))
